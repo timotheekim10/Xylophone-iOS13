aae329e52e093230ea0a90ece2361707b5a013e9
timotheekim10 <timotheekim10@gmail.com>
2023-06-30 21:35:33 +0900

    Link Multiple Buttons to the Same IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print(sender.currentTitle)
+        
         playSound()
+        
     }
     
     func playSound() {
